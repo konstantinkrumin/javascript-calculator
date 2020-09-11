906169889970ee4848ca404fc77c1dd70b2867bf
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 00:09:47 +0300

    Updates

---
+++ precache-manifest.500316b2631faf0388e6f25e3dfb94a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c11bcffa332b46d3607830db0250ea",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "260dcca127673d417995",
+    "url": "/javascript-calculator/static/css/main.4992a08d.chunk.css"
+  },
+  {
+    "revision": "dd59523c5985a032b37a",
+    "url": "/javascript-calculator/static/js/2.6928b069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.6928b069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260dcca127673d417995",
+    "url": "/javascript-calculator/static/js/main.8b265440.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
